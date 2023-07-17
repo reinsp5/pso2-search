114ce47c6a670bd651d4cfbe9b5b9758526a474d
レイン <sk.reinsp5@gmail.com>
2023-07-17 11:40:30 +0900

    文字数引き上げ

--- components/general/ItemNameField.vue
+++ components/general/ItemNameField.vue
@@ -2,12 +2,15 @@
 // 入力情報の共有State
 const itemInfo = useInsertItemInfo();
 
+// 文字数制限
+const textLength = 50;
+
 // 必須入力チェック
 const required = (v: string) => !!v || "必ず入力してください";
 
 // 文字数制限
 const limitLength = (v: string) =>
-  (v && v.length <= 20) || "20文字以内で入力してください";
+  (v && v.length <= textLength) || `${textLength}文字以内で入力してください`;
 </script>
 
 <template>
@@ -19,7 +22,7 @@ const limitLength = (v: string) =>
     variant="outlined"
     density="compact"
     :rules="[required, limitLength]"
-    counter="20"
+    :counter="textLength"
   />
 </v-col>
 </template>
